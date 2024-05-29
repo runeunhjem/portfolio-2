094c4491cd6128d663206be6aaf77a4e73e41232
runeunhjem <runeunhjem@gmail.com>
2024-05-29 08:25:44 +0200

    changes logo path

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className=" mx-auto flex w-full justify-between bg-orange-200 p-4 text-stone-600">
       <div className="header-container mx-auto flex w-full max-w-4xl justify-between">
         <img
-          src="/src/assets/logos/rundev-logo-in-brackets.svg"
+          src=".../../../../assets/logos/rundev-logo-in-brackets.svg"
           alt="RUNDEV Logo"
           className=" max-w-64"
         />
