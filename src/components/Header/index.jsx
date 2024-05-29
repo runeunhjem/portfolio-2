e6baae144cc80d67864dec2970a3e47ee8d163ae
runeunhjem <runeunhjem@gmail.com>
2024-05-29 08:30:57 +0200

    changes logo path

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,14 +3,14 @@ import NavBar from "../NavBar";
 
 const Header = () => {
   return (
-    <header className=" mx-auto flex w-full justify-between bg-orange-200 p-4 text-stone-600">
+    <header className="mx-auto flex w-full justify-between bg-orange-200 p-4 text-stone-600">
       <div className="header-container mx-auto flex w-full max-w-4xl justify-between">
         <img
-          src=".../../../../assets/logos/rundev-logo-in-brackets.svg"
+          src="/assets/logos/rundev-logo-in-brackets.svg"
           alt="RUNDEV Logo"
-          className=" max-w-64"
+          className="max-w-64"
         />
-        <div className="relative nav-container mx-auto flex w-full max-w-4xl flex-col items-end">
+        <div className="nav-container relative mx-auto flex w-full max-w-4xl flex-col items-end">
           <HamburgerAccordion />
           <NavBar />
         </div>
