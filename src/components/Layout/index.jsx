77117662c79ac1ae35e746dc687fa3ee9cc4e62f
runeunhjem <runeunhjem@gmail.com>
2024-06-05 15:02:22 +0200

    Performance - reserves height and width to avoid layout shifts

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,9 +4,14 @@ import Footer from "../Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-grow p-4">{children}</main>
+      <main
+        className="flex-grow p-4"
+        style={{ minHeight: "100vh", minWidth: "100%" }}
+      >
+        {children}
+      </main>
       <Footer />
     </div>
   );
