aac24413e3fb42baf0776a0cdb314cf376cdbaa1
runeunhjem <runeunhjem@gmail.com>
2024-06-06 15:18:37 +0200

    Removes minify and treeshake

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: "esbuild", // Ensures JavaScript is minified
-    rollupOptions: {
-      treeshake: true, // Ensures unused code is removed
-    },
-  },
-});
+})
