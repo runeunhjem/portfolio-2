61ab7c5aa015dc9f9b54c0874fce758e8dc6d499
runeunhjem <runeunhjem@gmail.com>
2024-06-06 06:13:10 +0200

    Adds lazy load of components to improve performance

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "../../pages/HomePage";
-import AboutPage from "../../pages/AboutPage";
-import ContactPage from "../../pages/ContactPage";
-import Layout from "../Layout";
+import { lazy, Suspense } from "react";
 import { CssBaseline } from "@mui/material";
+import Layout from "../Layout";
 import projectsData from "../ProjectGallery/projectsData";
+
+// Lazy load the page components
+const HomePage = lazy(() => import("../../pages/HomePage"));
+const AboutPage = lazy(() => import("../../pages/AboutPage"));
+const ContactPage = lazy(() => import("../../pages/ContactPage"));
 
 const totalProjects = projectsData.length;
 
@@ -13,14 +16,16 @@ const App = () => {
     <Router>
       <CssBaseline />
       <Layout>
-        <Routes>
-          <Route
-            path="/"
-            element={<HomePage numberOfProjects={totalProjects} />}
-          />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route
+              path="/"
+              element={<HomePage numberOfProjects={totalProjects} />}
+            />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </Router>
   );
